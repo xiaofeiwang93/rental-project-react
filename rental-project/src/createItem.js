c69f7e757029019ff5b5bf534ac3062e39d43a5a
Owen <xiaofei.wang93@gmail.com>
2020-02-05 11:44:40 +1300

    complete the create item page

--- rental-project/src/createItem.js
+++ rental-project/src/createItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Input, DatePicker, InputNumber, Row, Col } from 'antd';
+import { Input, DatePicker, InputNumber, Row, Col, Button } from 'antd'
+import './createItem.css'
+const { TextArea } = Input;
 
 const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 
@@ -8,40 +10,51 @@ function onChange(date, dateString) {
 }
 
 export default function createItem() {
-    const InputBox = props => <p className={`height-${props.value}`}>{props.children}</p>;
-
     return (
-        <div className="example-input">
-            <Row type="flex" justify="center">
-                <InputBox value={50}>
-                    <h1>Add a New Item</h1>
-                </InputBox>
+        <div className="create-new-page">
+            <Row type="flex" justify="center" gutter={[24, 24]}>
+                <div className="gutter-box"><h1>Add a New Item</h1></div>
             </Row>
-            <Row type="flex" justify="center">
+            <Row type="flex" justify="center" gutter={[24, 24]}>
                 <Col span={4}>
-                    <InputBox value={100}>
+                    <div className="gutter-box">
                         <Input placeholder="Name" size="large"/>
-                    </InputBox>
+                    </div>
                 </Col>
                 <Col span={4}>
-                    <InputNumber
-                        defaultValue={0}
-                        formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                        parser={value => value.replace(/\$\s?|(,*)/g, '')}
-                        onChange={onChange}
-                    />
+                    <div className="gutter-box">
+                        <InputNumber
+                            defaultValue={0}
+                            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={value => value.replace(/\$\s?|(,*)/g, '')}
+                            onChange={onChange}
+                            size="large"
+                            style={{width:"100%"}}
+                        />  
+                    </div>
                 </Col>
             </Row>
-            <Row type="flex" justify="center">
+            <Row type="flex" justify="center" gutter={[24, 24]}>
                 <Col span={4}>
-                    <Input placeholder="Image Url" size="large"/>
+                    <div className="gutter-box">
+                        <TextArea placeholder="Image Url" size="large" autoSize={{ minRows: 3}}/>
+                    </div>
                 </Col>
                 <Col span={4}>
-                    <Input placeholder="Description" size="large"/>
+                    <div className="gutter-box">
+                        <TextArea placeholder="Description" size="large" autoSize={{ minRows: 3}}/>
+                    </div>
                 </Col>
             </Row>
-            <Row type="flex" justify="center">
-                <DatePicker onChange={onChange} />
+            <Row type="flex" justify="center" gutter={[24, 24]}>
+                <div className="gutter-box">
+                    <DatePicker onChange={onChange} />
+                </div>
+            </Row>
+            <Row type="flex" justify="center" gutter={[24, 24]} style={{marginTop: "12px"}}>
+                <div className="gutter-box" >
+                    <Button type="primary">Submit</Button>
+                </div>
             </Row>
         </div>
     )
